a38eb7f77f2d85d54292be4d27b7b8a31a22ad9c
ErickBernat <erickduartebr@gmail.com>
2025-05-06 23:41:49 -0300

    Feat:Visual final

--- Scripts/validaExpressao.js
+++ Scripts/validaExpressao.js
@@ -1,3 +1,7 @@
+import { calculaFatorial } from "./calculaFatorial.js";
+import { calculaTangente, calculaTrigonometria } from "./calculaTrignonometria.js";
+
+
 const inputCalculadora = document.getElementById('inputCalculadora');
 const regexValidaOperador =  /[+%\-.!*^%/]/
 
@@ -5,61 +9,6 @@ const regexValidaOperador =  /[+%\-.!*^%/]/
 export function validaExpressao(expressao, tecla){
         validaOperadoresNaExpressao(expressao,tecla);
 }
-
-export function validaOperadoresNaExpressao(expressao,tecla){
-    let ultimoCaracter = expressao[expressao.length-1]
-    let ultimoCaracterOperador = regexValidaOperador.test(ultimoCaracter);
-    if(expressao == '' && tecla != '(' && tecla != '-' && tecla != '√('){
-        return
-    }
-    if(ultimoCaracter == '(' && tecla != ")"  && tecla != "(" &&tecla != "-"){
-        return
-    }
-    if(tecla == '(' && ultimoCaracterOperador && ultimoCaracter != ')'){
-        inputCalculadora.value +=`${tecla}`
-    }
-    if(ultimoCaracterOperador && ultimoCaracter != ')'&& ultimoCaracter != '('&& ultimoCaracter != '%'&& ultimoCaracter != '!'){
-        return
-    }
-    inputCalculadora.value +=`${tecla}`;
-}
-
-export function calculaFatorial(numero){
-    let respostaExpressao = new Function(`return ${numero}`);
-    numero = respostaExpressao()
-    let respostaFatorial = 1;
-    for(let contadorFatorial = 1 ; contadorFatorial <=numero ; contadorFatorial++){
-        respostaFatorial *=contadorFatorial;
-    }
-    return respostaFatorial;
-}
-
-export function calculaTrigonometria(tipoCalculo,valor){
-    const termos = 10;
-    let resultado=0;
-    let exponesialSeno=0;
-    let fatorialContador=0;
-    let calculoTermo=0;
-    let defineCalculo = tipoCalculo == 'seno' ? 1 : 0;
-    for(let contadorTermo=0 ; contadorTermo<termos;contadorTermo++){
-            const sinal = Math.pow(-1, contadorTermo);
-            exponesialSeno = valor**(2*contadorTermo+defineCalculo);
-            fatorialContador = calculaFatorial(2*contadorTermo+defineCalculo);
-            calculoTermo = sinal * (exponesialSeno/fatorialContador);
-            resultado +=  calculoTermo;
-    }
-    return resultado
-}
-
-
-export function calculaTangente(tangente){
-    let seno = calculaSeno(tangente)
-    let cosseno = calculaCosseno(tangente)
-    let resposta = seno/cosseno
-    return resposta
-}
-
-
 export function formataParenteses(expressao) {
     const abertura = (expressao.match(/\(/g) || []).length;
     const fechamento = (expressao.match(/\)/g) || []).length;
@@ -69,12 +18,12 @@ export function formataParenteses(expressao) {
 
 export function formataExpressao(expressao){
     expressao = formataParenteseMultiplica(expressao)
+    expressao = formataParenteses(expressao);
     expressao = formataSenCosTan(expressao)
     expressao = formataExponencial(expressao)
     expressao = formataRaiz(expressao)
     expressao = formataPotencia(expressao)
     expressao = formataFatorial(expressao)
-    expressao = formataParenteses(expressao);
     return expressao
 }
 
@@ -108,12 +57,38 @@ function formataRaiz(expressao){
     expressao = expressao.replaceAll("√","Math.sqrt");
     return expressao
 }
+
 function formataExponencial(expressao){
     return expressao.replaceAll("^","**");
 }
+
 function formataSenCosTan(expressao){
-    expressao = expressao.replaceAll("sin(","Math.sin(");
-    expressao = expressao.replaceAll("cos(","Math.cos(");
-    expressao = expressao.replaceAll("tan(","Math.tan(");
+    expressao = expressao.replace(/\bsin\s*\(\s*([^)]+?)\s*\)/g, (_, valor) => {
+        return  calculaTrigonometria('seno',valor);
+    });
+    expressao = expressao.replace(/\bcos\s*\(\s*([^)]+?)\s*\)/g, (_, valor) => {
+        return  calculaTrigonometria('cosseno',valor);
+    });
+    expressao = expressao.replace(/\btan\s*\(\s*([^)]+?)\s*\)/g, (_, valor) => {
+        return  calculaTangente(valor);
+    });
     return expressao
+}
+
+export function validaOperadoresNaExpressao(expressao,tecla){
+    let ultimoCaracter = expressao[expressao.length-1]
+    let ultimoCaracterOperador = regexValidaOperador.test(ultimoCaracter);
+    if(expressao == '' && tecla != '(' && tecla != '-' && tecla != '√('){
+        return
+    }
+    if(ultimoCaracter == '(' && tecla != ")"  && tecla != "(" &&tecla != "-"){
+        return
+    }
+    if(tecla == '(' && ultimoCaracterOperador && ultimoCaracter != ')'){
+        inputCalculadora.value +=`${tecla}`
+    }
+    if(ultimoCaracterOperador && ultimoCaracter != ')'&& ultimoCaracter != '('&& ultimoCaracter != '%'&& ultimoCaracter != '!'){
+        return
+    }
+    inputCalculadora.value +=`${tecla}`;
 }
--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-import { realizaConta } from "./Scripts/realizaConta.js";
-import { calculaTrigonometria,validaExpressao } from "./Scripts/validaExpressao.js";
 import { validaTeclaInput} from "./Scripts/validaTeclasInput.js";
 
-
 validaTeclaInput();
-calculaTrigonometria('sen',4);
